ae6d688dd50fe9836ca8556ff060d85cd02d53a8
SelfJ19 <SELFJ1@ETSU.EDU>
2022-09-21 12:15:50 -0400

    added regions to hand.cs

--- Project2/Hand.cs
+++ Project2/Hand.cs
@@ -1,1 +1,40 @@
-﻿
+﻿////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Project: Project 2 Hand-Deck-Cards
+// File Name: Hand.cs
+// Description: Models one single card out of an entire deck
+// Course: CSCI 1260-001 – Introduction to Computer Science II
+// Author: Jason Self, selfj1@etsu.edu, East Tennessee State University
+// Created: 9/21/2022
+// Copyright: Jason Self, 2022
+//
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+using System;
+namespace CardClass;
+
+public class Hand
+{
+    public Card GameHand { get; set; }
+    public int HandSize { get; set; }
+    public int CardsInHand { get; set; }
+
+    #region Hand()
+
+    #endregion
+
+    #region Hand(HandSize)
+
+    #endregion
+
+    #region Hand(existingHand)
+
+    #endregion
+
+    #region AddACard(Card)
+
+    #endregion
+
+    #region ToString()
+
+    #endregion
+}
